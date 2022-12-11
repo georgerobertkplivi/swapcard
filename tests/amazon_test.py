4acc75fa217ac45c215c52f9fe262deee12c51c5
George Robrt Kplivi <georgerobertkplivi@gmail.com>
2022-12-11 21:14:46 +0000

    refactored get_star_rating

--- tests/amazon_test.py
+++ tests/amazon_test.py
@@ -6,14 +6,14 @@ from tests.base_test import BaseTest
 class AmazonTest(BaseTest, ProductPage):
 
     ## Check the review score. If it's less than 4, fail the test, otherwise pass it
-    def test_product_rating_less_than_4(self):
+    def test_product_rating(self):
         self.select_beadind_and_jewelry_making()
         self.click_engraving_link()
         self.click_sort_by_dropdown()
         self.select_sort_by_type(SortBy.HIGH_TO_LOW.value)
         self.open_product(3)
         self.wait_for_product_page_to_load()
-        self.assertTrue(self.get_star_rating() < 4)
+        self.assertTrue(self.get_star_rating() >= 4)
 
     ##  Check the price for the opened item. If it's more than $4000, fail the test, otherwise pass it.
     def test_product_price_less_than_4000(self):
